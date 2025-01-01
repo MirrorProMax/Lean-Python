12ca7b22246f19b05bfd9de48cb3c88ace188ca4
MirrorPro <644328850@qq.com>
2025-01-01 22:57:36 +0800

    202501012257

--- 9-1-2-测试-生成斐波那契数列.py
+++ 9-1-2-测试-生成斐波那契数列.py
@@ -2,7 +2,7 @@ from typing import Generator
 
 def 斐波那契数列Generator(times)->Generator[int,None,None]:
     a,b=0,1#斐波那契数列的前两个数是0和1
-    yield a#先把0放入生成器
+    yield a#先把0放入生成器; 此时函数并不会停止
     yield b#再把1放入生成器
     for i in range(times-2):#因为已经有了0和1，所以只需要生成countNum-2个数
         a,b=b,a+b#斐波那契数列的规则是后一个数等于前两个数之和
